8e79c664f688a39c72c14a2bde351a461c3cea96
“baixue” <baixue@163.com>
2024-01-07 04:09:40 +0800

    TS中的接口、泛型、自定义类型

--- hello_vue3/src/components/TsDemo.vue
+++ hello_vue3/src/components/TsDemo.vue
@@ -1,9 +1,43 @@
 <template>
-    <div class="ts"></div>
+    <div class="ts">
+        <h2>{{ person.name }}</h2>
+        <ul>
+            <li v-for="item in personList" :key="item.id">
+                <p>id:{{ item.id }}</p>
+                <p>name:{{ item.name }}</p>
+                <p>age:{{ item.age }}</p>
+            </li>
+        </ul>
+        <ol>
+            <li v-for="item in personList2" :key="item.id">
+                <p>id:{{ item.id }}</p>
+                <p>name:{{ item.name }}</p>
+                <p>age:{{ item.age }}</p>
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script lang="ts" setup name="Ts-Demo">
+// @ 表示从src目录开始搜索
+// 导入类型PersonInter
+import { type PersonInter, type persons } from '@/types'
 
+// 声明变量为PersonInter类型，并且赋值
+let person: PersonInter = { id: '1', name: '张三', age: 60 }
+
+// 声明一个数组，数组里面每一项都是PersonInter类型
+let personList: Array<PersonInter> = [
+    { id: '1', name: '张三', age: 60 },
+    { id: '2', name: '李四', age: 80 },
+    { id: '3', name: '王五', age: 90 },
+]
+
+let personList2: persons = [
+    { id: '1', name: '张三', age: 60 },
+    { id: '2', name: '李四', age: 80 },
+    { id: '3', name: '王五', age: 90 },
+]
 </script>
 
 <style scoped>
---
+++ hello_vue3/src/types/index.ts
@@ -0,0 +1,10 @@
+// 定义一个接口，用户限制person对象的具体属性
+export interface PersonInter {
+    id: string;
+    name: string;
+    age: number;
+}
+
+// 定义一个类型，类型为数组，数组中的每一项类型都为PersonInter
+export type persons = Array<PersonInter>
+// 上述代码也可以写成这样的，export type persons = PersonInter[]
