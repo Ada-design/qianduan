11c347b977479f6dab5845c03b9e97e824378abd
“baixue” <baixue@163.com>
2024-01-08 00:29:23 +0800

    vue3的生命周期练习

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,21 +1,10 @@
 <template>
   <!-- html -->
-  <!-- :b表示属性b,等号里面的是表达式，a="1+1",里面是字符串，渲染出来的结果是a="1+1" -->
-  <!-- <h2 a="1+1" :b="1+1" c="x" :d="x"></h2> -->
-  <propsDemp a="哈哈" :list="personList" />
+  <lifecycleDemo/>
 </template>
 
 <script lang="ts" setup>
-import propsDemp from '@/components/propsDemo.vue';
-// let x = 9
-import { reactive } from 'vue'
-import { type Persons } from '@/types';
-
-let personList: Persons = reactive<Persons>([
-  { id: '1', name: '张三', age: 13 },
-  { id: '2', name: '李四', age: 14 },
-  { id: '3', name: '王五', age: 15 }
-])
+import lifecycleDemo from '@/components/lifecycleDemo.vue'
 </script>
 
 <style>
---
+++ hello_vue3/src/components/lifecycleDemo.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="lifecycle">
+        <h2>求和为{{ sum }}</h2>
+        <button @click="add">sum+1</button>
+    </div>
+</template>
+
+<script lang="ts" setup name="Lifecycle-Demo">
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+let sum = ref(0)
+
+function add() {
+    sum.value++
+}
+// 生命周期函数
+
+// vue2中的beforeCreate、created在vue3中使用了setup代替
+// 创建
+console.log(sum.value)
+
+// 挂载前
+onBeforeMount(() => {
+    console.log('挂载前')
+})
+
+// 挂载完毕
+onMounted(() => {
+    console.log('挂载完毕')
+})
+
+// 更新前
+onBeforeUpdate(() => {
+    console.log('更新前')
+})
+
+// 更新完毕
+onUpdated(() => {
+    console.log('更新完毕')
+})
+
+// 卸载前
+onBeforeUnmount(() => {
+    console.log('卸载前')
+})
+
+// 卸载完毕
+onUnmounted(() => {
+    console.log('卸载完毕')
+})
+</script>
+
+<style scoped>
+.lifecycle {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    background-color: rgb(0, 255, 162);
+    color: white;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    cursor: pointer;
+    margin-right: 10px;
+}
+</style>
